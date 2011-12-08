b41f9a0900b3882479f69834da0bbe54fc673727
Dan Melton <melton.dan@gmail.com>
2011-12-08 12:55:09 -0800

    Added coloring to progress bar on project page to indicate on schedule

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,16 @@
 module ProjectsHelper
+
+  def on_schedule(milestone_history) 
+    schedule = milestone_history.planned_date.to_time - Time.now
+    if !milestone_history.completed_date?
+      if schedule < -1209600
+        "red_schedule"
+      elsif schedule < 0
+        "yellow_schedule"
+      elsif schedule > 0
+        "green_schedule"
+      end
+    end
+  end
+
 end
