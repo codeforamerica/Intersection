6a1cb8a690d126a90a8ccb834d403aca2b03e9bb
Dan Melton <melton.dan@gmail.com>
2011-12-13 11:12:22 -0800

    Added dashboard sections to the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @teams = Team.active_teams
+    @fellows = Profile.where(:user_type => "Fellow")
+    @projects = Project.all
+    @network_size = @teams.map { |x| x.network_growth}
+    @project_history = ProjectMilestone.where("active =?", true).order("updated_at DESC")
   end
 
 end
