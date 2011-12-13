3ffeb88b9e0cc9c412ab71e26e14349c4a811f8b
Dan Melton <melton.dan@gmail.com>
2011-12-13 15:25:44 -0800

    Altered cities index to match home

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,6 +2,10 @@ class TeamsController < InheritedResources::Base
   before_filter :authenticate_user!, :except => [:show, :index]
   before_filter :is_admin?, :only => [:new, :create, :destroy]
 
+  def team
+    @teams = Team.active_teams
+  end
+
   def show
     @team = params[:id].to_i==0 ? Team.where(:name => params[:id]).first : Team.find(params[:id])
   end
