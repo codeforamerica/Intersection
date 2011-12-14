66b935acbf77b2b46ff4bfb8df47e492057bf5a6
Dan Melton <melton.dan@gmail.com>
2011-12-13 17:41:35 -0800

    Added charts to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ActiveRecord::Base
   belongs_to :team
   order("event_date DESC")
+
+  def self.grouped
+    group("date(events.event_date)").size
+  end
+  
+  def self.month_group
+    events=self.grouped.map { |x| {"#{x[0].to_date.strftime("%m/%y")}" =>  x[1]} }
+    events.inject{|memo, el| memo.merge( el ){|k, old_v, new_v| old_v + new_v}}
+  end
+
+
 end
