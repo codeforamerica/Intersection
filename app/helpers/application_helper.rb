7ee0c0b9d86def1390e04c1d2bc1b4ee6c0b74ba
Dan Melton <melton.dan@gmail.com>
2011-12-16 14:26:13 -0800

    Adding new trending for surveys to various display pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
     end
   end
 
+  def trend_r_y_g(scale, num)
+    case scale.to_i
+    when 1
+      "<div id='red'>"+num.to_s+"</div>"
+    when 2
+      "<div id='yellow'>"+num.to_s+"</div>"
+    when 3
+      "<div id='green'>"+num.to_s+"</div>"
+    when 0
+      ""
+    end
+  end
 end
--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -13,11 +13,34 @@ class SurveyResponse < ActiveRecord::Base
   def self.trends
     group("date(survey_responses.created_at)").average(:response)
   end
+  
+  def self.count_response_trends
+    group("date(survey_responses.created_at)").count(:response)
+  end
+
+  def self.trends_by_response(num=6)
+    date_array = SurveyResponse.group("date(created_at)").count("date(created_at)")
+    blank_array = date_array.clone
+    new_array = date_array.clone
+    blank_array.each {|k,v| blank_array[k]=0}
+    new_array.each {|k,v| new_array[k]=[0,0,0]}
+    one= self.where(:response => 1).count_response_trends
+    two= self.where(:response => 2).count_response_trends
+    three= self.where(:response => 3).count_response_trends
+    one = blank_array if one.empty?
+    two = blank_array if two.empty?
+    three = blank_array if three.empty?
+    one.each {|k,v| new_array[k][0]=v}
+    two.each {|k,v| new_array[k][1]=v}
+    three.each {|k,v| new_array[k][2]=v}
+    new_array.to_a[0..num].reverse 
+    
+  end
 
   def self.trending(num=6)
     s= self.trends
     t = s.to_a
-    t[0..6].reverse
+    t[0..num].reverse
   end
 
 end
