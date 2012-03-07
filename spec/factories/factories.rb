3721c8a9547a90fd3e817f6250dfbe7c294ae9c7
Ryan Resella <ryan@codeforamerica.org>
2012-03-06 18:03:28 -0800

    Update factory girl syntax

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,108 +6,107 @@ FactoryGirl.define do
     admin false
     password "something"
   end
-end
 
+  factory  :admin_user, :parent => :user do |u|
+    u.admin true
+  end
 
-Factory.define :admin_user, :parent => :user do |u|
-  u.admin true
-end
-
-Factory.define :profile do |profile|
-  profile.name { Faker::Name.name }
-  profile.bio {Faker::Lorem.sentence(3)}
-  profile.user_type "Fellow"
-end
+  factory :profile do |profile|
+    profile.name { Faker::Name.name }
+    profile.bio {Faker::Lorem.sentence(3)}
+    profile.user_type "Fellow"
+  end
 
 
 #--[Project]-------------
 
-Factory.define :project do |p|
-  p.name {Faker::Company.catch_phrase}
-  p.description {Faker::Lorem.paragraph}
-  p.impact_statement {Faker::Lorem.paragraph}
-  p.tag_list {Faker::Lorem.words(4).join(",")}
-end
+  factory :project do |p|
+    p.name {Faker::Company.catch_phrase}
+    p.description {Faker::Lorem.paragraph}
+    p.impact_statement {Faker::Lorem.paragraph}
+    p.tag_list {Faker::Lorem.words(4).join(",")}
+  end
 
-Factory.define :link_type do |lt|
-  lt.name "github"
-end
+  factory :link_type do |lt|
+    lt.name "github"
+  end
 
-Factory.define :link do |l|
-  l.name "Something"
-  l.url {Faker::Internet.domain_name}
-  l.project {Factory(:project)}
-  l.link_type {Factory(:link_type)}
-end
+  factory :link do |l|
+    l.name "Something"
+    l.url {Faker::Internet.domain_name}
+    l.project {Factory(:project)}
+    l.link_type {Factory(:link_type)}
+  end
 
 
-Factory.define :milestone do |m|
-  m.name {Faker::Lorem.words(4).to_sentence}
-end
+  factory :milestone do |m|
+    m.name {Faker::Lorem.words(4).to_sentence}
+  end
 
-Factory.define :project_milestone do |pm|
-  pm.milestone {Factory(:milestone)}
-  pm.project {Factory(:project)}
-  pm.planned_date {rand(6).months.ago}
-end
+  factory :project_milestone do |pm|
+    pm.milestone {Factory(:milestone)}
+    pm.project {Factory(:project)}
+    pm.planned_date {rand(6).months.ago}
+  end
 
 #----Teams--------
 
-Factory.define :team do |team|
-  team.name {Faker::Address.city}
-end
+  factory :team do |team|
+    team.name {Faker::Address.city}
+  end
 
-Factory.define :event do |e|
-  e.team {Factory(:team)}
-  e.name {Faker::Lorem.sentence}
-  e.attendance {rand(200)}
-  e.event_date {rand(15).weeks.ago}
-  e.url {Faker::Internet.domain_name}
-  e.description {Faker::Lorem.paragraph}
-end
+  factory :event do |e|
+    e.team {Factory(:team)}
+    e.name {Faker::Lorem.sentence}
+    e.attendance {rand(200)}
+    e.event_date {rand(15).weeks.ago}
+    e.url {Faker::Internet.domain_name}
+    e.description {Faker::Lorem.paragraph}
+  end
 
 #------Surveys---------------
 
-Factory.define :survey do |s|
-  s.name {Faker::Company.catch_phrase}
-  s.start_date {Time.now}
-  s.end_date {Time.now + 8.weeks}
-  s.frequency "1.weeks"
-  s.survey_type "Project"
-  s.scale 6
-end
+  factory :survey do |s|
+    s.name {Faker::Company.catch_phrase}
+    s.start_date {Time.now}
+    s.end_date {Time.now + 8.weeks}
+    s.frequency "1.weeks"
+    s.survey_type "Project"
+    s.scale 6
+  end
 
-Factory.define :survey_response do |r|
-  r.expires_on {Time.now + 1.week}
-  r.response 6
-  r.note {Faker::Lorem.sentence(2)}
-  r.survey {Factory(:survey)}
-  r.user {Factory(:user)}
-end
+  factory :survey_response do |r|
+    r.expires_on {Time.now + 1.week}
+    r.response 6
+    r.note {Faker::Lorem.sentence(2)}
+    r.survey {Factory(:survey)}
+    r.user {Factory(:user)}
+  end
 
-Factory.define :user_survey_response, :parent => :survey_response do |u|
-  u.surveyable {Factory(:user)}
-end
+  factory :user_survey_response, :parent => :survey_response do |u|
+    u.surveyable {Factory(:user)}
+  end
 
-Factory.define :project_survey_response do |u|
-  u.surveyable {Factory(:project)}
-end
+  factory :project_survey_response do |u|
+    u.surveyable {Factory(:project)}
+  end
 
-Factory.define :team_survey_response do |u|
-  u.surveyable {Factory(:team)}
-end
+  factory :team_survey_response do |u|
+    u.surveyable {Factory(:team)}
+  end
 
-Factory.define :batchbook_list do |l|
-  l.team {Factory(:team)}
-  l.batchbook_id {rand(100000)}
-end
+  factory :batchbook_list do |l|
+    l.team {Factory(:team)}
+    l.batchbook_id {rand(100000)}
+  end
 
-Factory.define :batchbook_list_history do |h|
-  h.contact_number {rand(10000)}
-  h.batchbook_list {Factory(:batchbook_list)}
-end
+  factory :batchbook_list_history do |h|
+    h.contact_number {rand(10000)}
+    h.batchbook_list {Factory(:batchbook_list)}
+  end
 
-Factory.define :story do |s|
-  s.user {Factory(:user)}
-  s.story {Faker::Lorem.paragraphs(2)}
+  factory :story do |s|
+    s.user {Factory(:user)}
+    s.story {Faker::Lorem.paragraphs(2)}
+  end
 end
