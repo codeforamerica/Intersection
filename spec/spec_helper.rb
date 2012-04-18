0f607bcdb1fc897f1b03e8f5c352e5380ef7b3bf
Ryan Resella <ryan@codeforamerica.org>
2012-04-18 14:09:08 -0700

    Fix simplecov coverage for rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,16 +6,17 @@ Spork.prefork do
   # need to restart spork for it take effect.
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'simplecov'
-  SimpleCov.start
   require 'webmock/rspec'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-  #Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each {|f| require f} 
+  #Dir["#{File.dirname(__FILE__)}/factories/*.rb"].each {|f| require f}
 
   RSpec.configure do |config|
     # == Mock Framework
@@ -49,4 +50,4 @@ end
 def fixture(file)
   File.new(fixture_path + '/' + file)
 end
-  
+
