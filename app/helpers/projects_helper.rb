7c76253b0b968765253f92b91cd33804052ab44d
Joel Mahoney <joelmahoney@gmail.com>
2012-06-27 11:55:47 -0700

    Fix on_schedule helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,14 +1,16 @@
 module ProjectsHelper
 
-  def on_schedule(milestone_history) 
-    schedule = milestone_history.planned_date.to_time - Time.now
-    if !milestone_history.completed_date?
-      if schedule < -1209600
-        "red_schedule"
-      elsif schedule < 0
-        "yellow_schedule"
-      elsif schedule > 0
-        "green_schedule"
+  def on_schedule(milestone_history)
+    if milestone_history.completed_date.blank?
+      if milestone.planned_date.present?
+        schedule = milestone_history.planned_date.to_time - Time.now        
+        if schedule < -1209600
+          "red_schedule"
+        elsif schedule < 0
+          "yellow_schedule"
+        elsif schedule > 0
+          "green_schedule"
+        end
       end
     end
   end
