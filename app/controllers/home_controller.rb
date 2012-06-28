aba69184f37129ec0e130824eb035e0fae459ec2
Joel Mahoney <joelmahoney@gmail.com>
2012-06-27 23:04:43 -0700

    Sort home page list by updated_at

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @teams = Team.active_teams
     @fellows = Profile.where(:user_type => "Fellow")
-    @projects = Project.all
+    @projects = Project.order('updated_at DESC')
     @events = Event.all
     @network_size = @teams.map { |x| x.network_growth}
     @project_history = ProjectMilestone.where("active =?", true).order("updated_at DESC")
