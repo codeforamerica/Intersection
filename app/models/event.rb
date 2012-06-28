391eb8a29678dbe17cb434484fb23bd6838790b1
Joel Mahoney <joelmahoney@gmail.com>
2012-06-27 23:10:38 -0700

    Fix to_date when nil

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,7 @@ class Event < ActiveRecord::Base
   end
 
   def self.month_group
-    events=self.grouped.map { |x| {"#{x[0].to_date.strftime("%m/%y")}" =>  x[1]} }
+    events=self.grouped.map { |x| {"#{x[0].try(:to_date).try(:strftime, "%m/%y")}" =>  x[1]} }
     events.inject{|memo, el| memo.merge( el ){|k, old_v, new_v| old_v + new_v}}
   end
 
